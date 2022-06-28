234079a9f0049d21ead5b11571663099343de53e
Ritik <varribhar@gmail.com>
2022-06-28 23:30:58 +0530

    upated

--- list.py
+++ list.py
@@ -26,3 +26,16 @@ print(list[-1])
 print(list[-2])
 print(list[-3])
 print(list[-5])
+
+
+# + and * operator 
+# concatination and multiplication
+
+list1 = [1,2,3,4,5]
+
+list1=list1+[6,7,8,9]
+print(list1)
+
+list2 = ["a","b","c"]
+
+print(list2*3)
