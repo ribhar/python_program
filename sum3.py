ace874025c7055ef86ac37c4cd695f28a8d4a0ae
Ritik <varribhar@gmail.com>
2022-06-28 22:22:02 +0530

    update

---
+++ sum3.py
@@ -0,0 +1,3 @@
+sum = lambda a,b,c: a+b+c
+
+print(sum(1,2,3))
---
+++ sumOfNatural.py
@@ -0,0 +1,8 @@
+def calc_sum(n):
+    if n ==1:
+        return 1
+    else:
+        return n + calc_sum(n-1)
+
+sum =calc_sum(5)
+print(sum)
