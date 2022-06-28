00c3ed6183f166491d450d4f3380222e5f3c2e2d
Ritik <varribhar@gmail.com>
2022-06-29 00:32:26 +0530

    upated

---
+++ tuples.py
@@ -0,0 +1,8 @@
+
+
+# a tuple is similar to a list. the difference between the two is that we cannot change the elements of a tuple once
+# it is assigned whereas, in list elements can be changed.
+
+# tuples can be created placing all the items (elements) inside parentheses (), separated by commas.
+
+# A tuples can have any number of items and the may be  different types (integer,float,list,sting etc.).
