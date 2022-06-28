46cb15ee411bee845eb6a04002da274faa85f589
Ritik <varribhar@gmail.com>
2022-06-28 23:26:26 +0530

    updated

--- list.py
+++ list.py
@@ -1,3 +1,5 @@
+
+# append extend
 odd  = [1,3,5]
 
 odd.append(3)
@@ -9,8 +11,18 @@ print(odd)
 
 my_list = ["p","y","t","h","o","n"]
 
+
+# slicing 
 print(my_list[1:3])
 print(my_list[:3])
 print(my_list[2:])
 print(my_list[:])
  
+# negative indexing
+
+list = [1,2,3,4,5]
+
+print(list[-1])
+print(list[-2])
+print(list[-3])
+print(list[-5])
