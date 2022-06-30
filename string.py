4fffa41d125dce94c4f8bed5acbb5a6744ac505f
Ritik <varribhar@gmail.com>
2022-06-30 16:05:51 +0530

    updated

--- string.py
+++ string.py
@@ -1,2 +1,9 @@
 # strings can be created by enclosing characters inside a single quote or double quotes. 
 # Evan triple quotes can be used in Python  but it is generally use to represent multiple lines.
+
+str = 'Hello world'
+print(str)
+
+str = "hello world"
+print(str)
+
