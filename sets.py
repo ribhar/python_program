661359e4990fa22367b689b9822636ed68eea75c
Ritik <varribhar@gmail.com>
2022-07-02 02:22:31 +0530

    added

---
+++ sets.py
@@ -0,0 +1,10 @@
+# A set is an unordered collection of items. Every element is unique (no duplication) and must be 
+# immutable (which cannot be changed).
+
+# A set is created by placing all the items (elements) inside curly braces{},
+# sperated by commas.
+# You can also use built in function set() to create sets.
+
+# sets of integers
+myset = {1,3,5}
+print(myset)
